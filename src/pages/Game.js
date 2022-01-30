13f6abff411fceb2408b05144644040f25b6b67a
livramateus <livra.matheus@gmail.com>
2022-01-30 18:13:11 -0300

    Added game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import PageTitle from '../components/PageTitle';
+import DataTable from '../components/DataTable';
+
 function Game() {
+
+    const [rows, setRows] = useState(null);
+    const labels = ['Name', 'Release Date', 'Publisher', 'Genre'];
+    const cols   = ['name', 'release_date', 'publisher', 'genre'];
+    const title  = "Games";
+
+    useEffect(() => {
+        fetchPublishers();
+    }, []);
+
+    const fetchPublishers = () => {
+        axios.get(`${process.env.REACT_APP_API_URL}/?page=game&action=getall`).then((res) => {
+            if (res.status === 200) {
+                setRows(res.data.data);
+            }
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     return (
-        <h3>Game</h3>
+        <main id="main">
+            <div className="content">
+                <PageTitle title={title} />
+                
+                <h1>{title}</h1>
+
+                <div className="contentstuff">
+                    <DataTable rows={rows} labels={labels} cols={cols} />
+                </div>
+            </div>
+        </main>
     );
 }
 
